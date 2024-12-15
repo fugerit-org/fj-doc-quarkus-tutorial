93396a7db075388c077f594d7191ee59cd1a9db1
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-12-15 01:04:57 +0100

    Init with plugin version 8.11.5

---
+++ src/main/java/org/fugerit/java/demo/fjdocquarkustutorial/People.java
@@ -0,0 +1,20 @@
+// generated from template 'People.ftl' on 2024-12-15T01:03:35.623+01:00
+package org.fugerit.java.demo.fjdocquarkustutorial;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+/*
+* Class used to wrap data to be rendered in the document template
+*/
+@Getter
+@AllArgsConstructor
+public class People {
+
+    private String name;
+
+    private String surname;
+
+    private String title;
+
+}
