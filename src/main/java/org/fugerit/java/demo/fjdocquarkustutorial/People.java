00e88c79d8d7d0ffd69ee8de8d6f9d810f2e66c0
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-03-04 00:06:03 +0100

    Project init
    mvn org.fugerit.java:fj-doc-maven-plugin:init \
    -DgroupId=org.fugerit.java.demo \
    -DartifactId=fj-doc-quarkus-tutorial \
    -Dextensions=base,freemarker,mod-poi \
    -Dflavour=quarkus-3

---
+++ src/main/java/org/fugerit/java/demo/fjdocquarkustutorial/People.java
@@ -0,0 +1,20 @@
+// generated from template 'People.ftl' on 2025-03-04T00:04:46.597+01:00
+package org.fugerit.java.demo.fjdocquarkustutorial;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+/*
+* Class used to wrap data to be rendered in the document template
+*/
+@Getter
+@AllArgsConstructor
+public class People {
+
+    private String name;
+
+    private String surname;
+
+    private String title;
+
+}
